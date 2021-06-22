81a067b660a9931c8870c9e655947a5e44da2b79
YuLili-git <72510006+YuLili-git@users.noreply.github.com>
2021-06-22 22:42:22 +0100

    Update 剑指 Offer 40. 最小的k个数.py

--- 剑指 Offer 40. 最小的k个数.py
+++ 剑指 Offer 40. 最小的k个数.py
@@ -5,7 +5,28 @@
 #示例 2：
 #输入：arr = [0,1,2,1], k = 1
 #输出：[0]
+##########################solution1##########################
 class Solution:
     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
         sort_arr = sorted(arr)
         return sort_arr[:k]
+    
+##########################solution2##########################    
+class Solution:
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        def quick_sort(arr, l, r):
+            if r <= l:
+                return
+            i = l
+            j = r
+            while i < j:   
+                while i < j and arr[j] >= arr[l]:
+                    j -= 1
+                while i < j and arr[i] <= arr[l]:
+                    i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+            arr[l], arr[i] = arr[i], arr[l]
+            quick_sort(arr, l, i-1)
+            quick_sort(arr, i+1, r)
+        quick_sort(arr, 0, len(arr) - 1)
+        return arr[:k]
