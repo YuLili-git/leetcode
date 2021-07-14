ace7b009b51b2223eb75a27b0138728a323a6d0b
YuLili-git <72510006+YuLili-git@users.noreply.github.com>
2021-07-14 23:17:28 +0100

    Create 剑指 Offer 65. 不用加减乘除做加法.py

---
+++ 剑指 Offer 65. 不用加减乘除做加法.py
@@ -0,0 +1,17 @@
+#写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。
+#示例:
+#输入: a = 1, b = 1
+#输出: 2
+#提示：
+#a, b 均可能是负数或 0
+#结果不会溢出 32 位整数
+class Solution:
+    def add(self, a: int, b: int) -> int:
+        x = 0xffffffff
+        a, b = a & x, b & x
+        while b != 0:
+            a, b = (a ^ b), (a & b) << 1 & x
+        if a <= 0x7fffffff:
+            return a
+        else:
+            return ~(a ^ x)
